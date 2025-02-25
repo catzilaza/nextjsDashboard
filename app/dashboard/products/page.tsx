94b9ae1d7be4fb1687769df0180a4248fed6a48c
unknown <catzilaza@gmail.com>
2025-02-26 00:26:27 +0700

    product page error userSearchParams

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -10,7 +10,7 @@ export default async function Page() {
         <h1 className={`${lusitana.className} text-2xl`}>Products</h1>
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        <Search placeholder="Search products..." />
+        {/* <Search placeholder="Search products..." /> */}
         <CreateProduct />
       </div>
       {/* <Suspense>
