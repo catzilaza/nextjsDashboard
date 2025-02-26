5375773db2f800b523b68e4a0ebf6e973b07383c
unknown <catzilaza@gmail.com>
2025-02-26 13:13:28 +0700

    product page useSearchparams

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -2,20 +2,28 @@ import { lusitana } from "@/app/ui/fonts";
 import { Suspense } from "react";
 import Search from "@/app/ui/search";
 import { CreateProduct } from "@/app/ui/products/button";
-
-export default async function Page() {
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
         <h1 className={`${lusitana.className} text-2xl`}>Products</h1>
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        {/* <Search placeholder="Search products..." /> */}
+        <Search placeholder="Search products..." />
         <CreateProduct />
       </div>
-      {/* <Suspense>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <h1>Cards Products</h1>
-      </Suspense> */}
+      </Suspense>
       <div className="mt-5 flex w-full justify-center">
         <h1>Paginations</h1>
       </div>
--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -1,12 +1,21 @@
 import Link from "next/link";
 import NavLinks from "@/app/ui/nav-links";
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import {
+  ArrowRightIcon,
+  IdentificationIcon,
+} from "@heroicons/react/24/outline";
 
 const Navbar = () => {
   return (
     <div className="flex h-20 flex-row px-3 py-4 md:px-2">
       <div className="flex space-x-2">
         <NavLinks />
+        <Link
+          href="/login"
+          className="flex h-[48px] w-auto grow items-center justify-center gap-2 rounded-md bg-blue-300 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+        >
+          <span>Sign in</span> <IdentificationIcon className="w-5 md:w-6" />
+        </Link>
         <Link
           href="/login"
           className="flex h-[48px] w-auto grow items-center justify-center gap-2 rounded-md bg-blue-300 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
