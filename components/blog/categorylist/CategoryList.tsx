190d4dac64e7e9a23adbca3b93f856040b26d2d7
unknown <catzilaza@gmail.com>
2025-05-06 21:09:36 +0700

    blog at 7

--- components/blog/categorylist/CategoryList.tsx
+++ components/blog/categorylist/CategoryList.tsx
@@ -15,45 +15,45 @@ const getData = async () => {
 };
 
 const CategoryList = async () => {
-  const data = await getData();
-  // const data = [
-  //   {
-  //     _id: "1",
-  //     title: "Fashion",
-  //     slug: "fashion",
-  //     img: "/blog/fashion.png",
-  //   },
-  //   {
-  //     _id: "2",
-  //     title: "Lifestyle",
-  //     slug: "lifestyle",
-  //     img: "/blog/style.png",
-  //   },
-  //   {
-  //     _id: "3",
-  //     title: "Travel",
-  //     slug: "travel",
-  //     img: "/blog/travel.png",
-  //   },
-  //   {
-  //     _id: "4",
-  //     title: "Food",
-  //     slug: "food",
-  //     img: "/blog/food.png",
-  //   },
-  //   {
-  //     _id: "5",
-  //     title: "Fitness",
-  //     slug: "fitness",
-  //     img: "/blog/tiktok.png",
-  //   },
-  //   {
-  //     _id: "6",
-  //     title: "Technology",
-  //     slug: "technology",
-  //     img: "/blog/youtube.png",
-  //   },
-  // ];
+  // const data = await getData();
+  const data = [
+    {
+      _id: "1",
+      title: "Fashion",
+      slug: "fashion",
+      img: "/blog/fashion.png",
+    },
+    {
+      _id: "2",
+      title: "Lifestyle",
+      slug: "lifestyle",
+      img: "/blog/style.png",
+    },
+    {
+      _id: "3",
+      title: "Travel",
+      slug: "travel",
+      img: "/blog/travel.png",
+    },
+    {
+      _id: "4",
+      title: "Food",
+      slug: "food",
+      img: "/blog/food.png",
+    },
+    {
+      _id: "5",
+      title: "Fitness",
+      slug: "fitness",
+      img: "/blog/tiktok.png",
+    },
+    {
+      _id: "6",
+      title: "Technology",
+      slug: "technology",
+      img: "/blog/youtube.png",
+    },
+  ];
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Popular Categories</h1>
