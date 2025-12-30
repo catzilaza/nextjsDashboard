d3cc46982f5a2df7c410e8435fb66e01a7bc097a
unknown <catzilaza@gmail.com>
2025-12-31 05:20:31 +0700

    update api checkout at 1

--- app/api/ecommerce/stripe/create-payment-intent/route.ts
+++ app/api/ecommerce/stripe/create-payment-intent/route.ts
@@ -4,9 +4,9 @@ import Stripe from "stripe";
 // const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 //   apiVersion: "2024-12-18.acacia",
 // });
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-12-15.clover",
-});
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+//   apiVersion: "2025-12-15.clover",
+// });
 
 const calculateOrderAmount = (items: any) => {
   // Calculate the order total on the server to prevent
@@ -19,20 +19,20 @@ const calculateOrderAmount = (items: any) => {
 };
 
 export async function POST(req: Request) {
-  const { items } = await req.json();
+  // const { items } = await req.json();
 
   // Create a PaymentIntent with the order amount and currency
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: calculateOrderAmount(items),
-    currency: "usd",
-    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
-    automatic_payment_methods: {
-      enabled: true,
-    },
-  });
+  // const paymentIntent = await stripe.paymentIntents.create({
+  //   amount: calculateOrderAmount(items),
+  //   currency: "usd",
+  // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+  // automatic_payment_methods: {
+  //   enabled: true,
+  //   },
+  // });
 
-  return NextResponse.json({ clientSecret: paymentIntent.client_secret });
-  // return NextResponse.json({});
+  // return NextResponse.json({ clientSecret: paymentIntent.client_secret });
+  return NextResponse.json({});
 }
 
 // import { NextResponse } from "next/server";
