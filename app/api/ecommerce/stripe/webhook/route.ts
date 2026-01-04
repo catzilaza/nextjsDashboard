63b2959b9bd470d39544f1316b8ae3e84836f186
unknown <catzilaza@gmail.com>
2026-01-04 18:01:40 +0700

    dropbox at 1

--- app/api/ecommerce/stripe/webhook/route.ts
+++ app/api/ecommerce/stripe/webhook/route.ts
@@ -1,163 +1,163 @@
-import { getLoginSession } from "@/app/ecommerce/lib/uitls";
-import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/app/ecommerce/lib/prisma";
-import Stripe from "stripe";
+// import { getLoginSession } from "@/app/ecommerce/lib/uitls";
+// import { NextRequest, NextResponse } from "next/server";
+// import prisma from "@/app/ecommerce/lib/prisma";
+// import Stripe from "stripe";
 
-const db = prisma;
+// const db = prisma;
 
-const stripeKey = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-12-15.clover",
-});
-const stripeWebhook = new Stripe(process.env.STRIPE_WEBHOOK_SECRET!, {
-  apiVersion: "2025-12-15.clover",
-});
+// const stripeKey = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+//   apiVersion: "2025-12-15.clover",
+// });
+// const stripeWebhook = new Stripe(process.env.STRIPE_WEBHOOK_SECRET!, {
+//   apiVersion: "2025-12-15.clover",
+// });
 
-if (!stripeKey || !stripeWebhook) {
-  throw new Error("Stripe secret key must be provided");
-}
+// if (!stripeKey || !stripeWebhook) {
+//   throw new Error("Stripe secret key must be provided");
+// }
 
-const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET!;
+// const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET!;
 
-export async function POST(request: NextRequest) {
-  const body = await request.text();
-  const signature = request.headers.get("stripe-signature")!;
+// export async function POST(request: NextRequest) {
+//   const body = await request.text();
+//   const signature = request.headers.get("stripe-signature")!;
 
-  let event: Stripe.Event;
+//   let event: Stripe.Event;
 
-  try {
-    event = Stripe.webhooks.constructEvent(
-      // rawbody,
-      body,
-      signature,
-      STRIPE_WEBHOOK_SECRET
-    );
-  } catch (error) {
-    console.error("Webhook signature verification failed: ", error);
-    return NextResponse.json({ error: "Invalid Signature" }, { status: 400 });
-  }
+//   try {
+//     event = Stripe.webhooks.constructEvent(
+//       // rawbody,
+//       body,
+//       signature,
+//       STRIPE_WEBHOOK_SECRET
+//     );
+//   } catch (error) {
+//     console.error("Webhook signature verification failed: ", error);
+//     return NextResponse.json({ error: "Invalid Signature" }, { status: 400 });
+//   }
 
-  try {
-    switch (event.type) {
-      case "checkout.session.completed": {
-        const session = event.data.object as Stripe.Checkout.Session;
-        console.log(`Payment successful for session ID: ${session.id}`);
+//   try {
+//     switch (event.type) {
+//       case "checkout.session.completed": {
+//         const session = event.data.object as Stripe.Checkout.Session;
+//         console.log(`Payment successful for session ID: ${session.id}`);
 
-        const totalAmount = session.amount_total ?? 0;
+//         const totalAmount = session.amount_total ?? 0;
 
-        try {
-          const productId = session.metadata?.productId;
-          const quantity = session.metadata?.quantity;
+//         try {
+//           const productId = session.metadata?.productId;
+//           const quantity = session.metadata?.quantity;
 
-          // 1. Find product in the database using Prisma
-          const productData = await prisma.product.findUnique({
-            where: {
-              id: productId,
-            },
-            select: {
-              id: true,
-              price: true,
-            },
-          });
+//           // 1. Find product in the database using Prisma
+//           const productData = await prisma.product.findUnique({
+//             where: {
+//               id: productId,
+//             },
+//             select: {
+//               id: true,
+//               price: true,
+//             },
+//           });
 
-          if (!productData) {
-            console.error(`Product with ID ${productId} not found.`);
-            return NextResponse.json(
-              { error: "Webhook Error: Product not found" },
-              { status: 404 }
-            );
-          }
-        } catch (error) {
-          console.error("Error fetching product from database:", error);
-          const dErr = error as Error;
-          return NextResponse.json(
-            { error: `Webhook Error ${dErr.message}` },
-            { status: 500 }
-          );
-        }
-        break;
+//           if (!productData) {
+//             console.error(`Product with ID ${productId} not found.`);
+//             return NextResponse.json(
+//               { error: "Webhook Error: Product not found" },
+//               { status: 404 }
+//             );
+//           }
+//         } catch (error) {
+//           console.error("Error fetching product from database:", error);
+//           const dErr = error as Error;
+//           return NextResponse.json(
+//             { error: `Webhook Error ${dErr.message}` },
+//             { status: 500 }
+//           );
+//         }
+//         break;
 
-        // const userId = session.metadata?.userId;
-        // const priceId = session.metadata?.priceId;
+//         // const userId = session.metadata?.userId;
+//         // const priceId = session.metadata?.priceId;
 
-        // if (userId && priceId) {
-        //   const subscription = await Stripe.subscriptions.retrieve(
-        //     session.subscription as string
-        //   );
+//         // if (userId && priceId) {
+//         //   const subscription = await Stripe.subscriptions.retrieve(
+//         //     session.subscription as string
+//         //   );
 
-        //   await db.user.update({
-        //     where: { id: userId },
-        //     data: {
-        //       stripeSubscriptionId: subscription.id,
-        //       stripePriceId: priceId,
-        //       stripeCurrentPeriodEnd: new Date(
-        //         subscription.items.data[0].current_period_end * 1000
-        //       ),
-        //       plan: priceId === "premium" ? "premium" : "pro",
-        //     },
-        //   });
-        // }
-        // break;
-      }
-      // case "customer.subscription.updated": {
-      //   const subscription = event.data.object as Stripe.Subscription;
-      //   const customerId = subscription.customer as string;
+//         //   await db.user.update({
+//         //     where: { id: userId },
+//         //     data: {
+//         //       stripeSubscriptionId: subscription.id,
+//         //       stripePriceId: priceId,
+//         //       stripeCurrentPeriodEnd: new Date(
+//         //         subscription.items.data[0].current_period_end * 1000
+//         //       ),
+//         //       plan: priceId === "premium" ? "premium" : "pro",
+//         //     },
+//         //   });
+//         // }
+//         // break;
+//       }
+//       // case "customer.subscription.updated": {
+//       //   const subscription = event.data.object as Stripe.Subscription;
+//       //   const customerId = subscription.customer as string;
 
-      //   const user = await db.user.findFirst({
-      //     where: { stripeCustomerId: customerId },
-      //   });
+//       //   const user = await db.user.findFirst({
+//       //     where: { stripeCustomerId: customerId },
+//       //   });
 
-      //   if (user) {
-      //     await db.user.update({
-      //       where: { id: user.id },
-      //       data: {
-      //         stripeCurrentPeriodEnd: new Date(
-      //           subscription.items.data[0].current_period_end * 1000
-      //         ),
-      //         plan:
-      //           subscription.status === "active"
-      //             ? user.stripePriceId === "premium"
-      //               ? "premium"
-      //               : "pro"
-      //             : "free",
-      //       },
-      //     });
-      //   }
-      //   break;
-      // }
+//       //   if (user) {
+//       //     await db.user.update({
+//       //       where: { id: user.id },
+//       //       data: {
+//       //         stripeCurrentPeriodEnd: new Date(
+//       //           subscription.items.data[0].current_period_end * 1000
+//       //         ),
+//       //         plan:
+//       //           subscription.status === "active"
+//       //             ? user.stripePriceId === "premium"
+//       //               ? "premium"
+//       //               : "pro"
+//       //             : "free",
+//       //       },
+//       //     });
+//       //   }
+//       //   break;
+//       // }
 
-      // case "customer.subscription.deleted": {
-      //   const subscription = event.data.object as Stripe.Subscription;
-      //   const customerId = subscription.customer as string;
+//       // case "customer.subscription.deleted": {
+//       //   const subscription = event.data.object as Stripe.Subscription;
+//       //   const customerId = subscription.customer as string;
 
-      //   const user = await db.user.findFirst({
-      //     where: { stripeCustomerId: customerId },
-      //   });
+//       //   const user = await db.user.findFirst({
+//       //     where: { stripeCustomerId: customerId },
+//       //   });
 
-      //   if (user) {
-      //     await db.user.update({
-      //       where: { id: user.id },
-      //       data: {
-      //         stripeSubscriptionId: null,
-      //         stripePriceId: null,
-      //         stripeCurrentPeriodEnd: null,
-      //         plan: "free",
-      //       },
-      //     });
-      //   }
-      //   break;
-      // }
-      default:
-        console.log(`Unhandled event type: ${event.type}`);
-    }
-    return NextResponse.json({ received: true });
-  } catch (error) {
-    console.error("Error processing webhook: ", error);
-    return NextResponse.json(
-      { error: "Internal Server Error processing webhook" },
-      { status: 500 }
-    );
-  }
-}
+//       //   if (user) {
+//       //     await db.user.update({
+//       //       where: { id: user.id },
+//       //       data: {
+//       //         stripeSubscriptionId: null,
+//       //         stripePriceId: null,
+//       //         stripeCurrentPeriodEnd: null,
+//       //         plan: "free",
+//       //       },
+//       //     });
+//       //   }
+//       //   break;
+//       // }
+//       default:
+//         console.log(`Unhandled event type: ${event.type}`);
+//     }
+//     return NextResponse.json({ received: true });
+//   } catch (error) {
+//     console.error("Error processing webhook: ", error);
+//     return NextResponse.json(
+//       { error: "Internal Server Error processing webhook" },
+//       { status: 500 }
+//     );
+//   }
+// }
 
 // import { NextApiRequest, NextApiResponse } from "next";
 // import Stripe from "stripe";
